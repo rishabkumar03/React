96d4eec1f5b3b1f434015809f8e46f41ab4bc649
rishabkumar03 <rishabkumar3.2005@gmail.com>
2026-02-04 16:10:17 +0530

    feat: Initialize React Router project with Tailwind CSS
    - Add App.css and index.css to import Tailwind CSS
    - Create main application structure in App.jsx
    - Implement Layout component with Header and Footer
    - Add About, Contact, Home, User, and Github components
    - Set up routing with react-router-dom
    - Create Github component to fetch and display user data
    - Add Footer and Header components with navigation links
    - Configure Vite for React and Tailwind CSS

---
+++ 07reactRouter/src/components/User/User.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { useParams } from 'react-router-dom'
+
+function User() {
+    const {userId} = useParams()
+    return (
+        <div className='bg-gray-700 text-white text-3xl p-3'>
+            User: {userId}
+        </div>
+    ) 
+}
+
+export default User
